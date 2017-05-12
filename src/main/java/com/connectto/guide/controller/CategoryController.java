daa76ed46f8f9cd4a3a21aeeefe3f0b4fecc88be
serozh <serozh@connectto.com>
2017-05-12 14:40:34 +0400

    clean imports , category, Constatn

--- src/main/java/com/connectto/guide/controller/CategoryController.java
+++ src/main/java/com/connectto/guide/controller/CategoryController.java
@@ -8,29 +8,33 @@ import com.connectto.guide.service.CategoryService;
 import com.connectto.guide.service.util.ServiceHelper;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CategoryController {
 
     private static final Logger logger = Logger.getLogger(CategoryController.class.getSimpleName());
 
-    private ResponseDto responseDto = new ResponseDto();
+    @Autowired
+    private ResponseDto responseDto;
 
     @Autowired
     private CategoryService service;
 
-    @RequestMapping(path = "m-channel-categories.htm", method = RequestMethod.GET)
+    @RequestMapping(path = "m-channel-categories", method = RequestMethod.GET)
     public ResponseDto mobileChannelCategories() {
+
+        responseDto.cleanMessages();
 
         int partitionId = ServiceHelper.getAuthenticatedUser().getPartitionId();
 
         if (partitionId == 0) {
-            responseDto.addFieldError("sessionId", "Invalid sessionId");
+            responseDto.setActionerror("Invalid sessionId");
+            responseDto.setStatus(ResponseStatus.RESOURCE_NOT_FOUND);
             return responseDto;
         }
 
